9b416789f488528110d33b0364de36e3fe32f352
slackfx <nandogimenes@gmail.com>
2016-08-01 21:15:28 -0300

    Initial Commit

---
+++ HelloCoreData/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelloCoreData
+//
+//  Created by Fernando Xavier on 01/08/16.
+//  Copyright © 2016 Slackfx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
